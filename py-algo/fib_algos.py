6724f4bd78cf8b11ed3848da1b0f014a20e11458
James Beedy <jbeedy@jbeedy-laptop.local>
2015-09-05 12:23:59 -0700

    Modified fib_algos.py

--- py-algo/fib_algos.py
+++ py-algo/fib_algos.py
@@ -11,3 +11,8 @@ def fib_recursive(n):
     else:
         return fib_recursive(n-1) + fib_recursive(n-2)
 
+if __name__ == "__main__":
+    # Get fib_num input from user
+    fib_num = int(raw_input("Enter a number between 1 and 30"))
+    print(fib_recursive(fib_num))
+
